6e8f4f84b5a84c51cca7f3f7ddf8a08d1af08982
WangFaRui <970201006@qq.com>
2022-02-15 18:05:02 +0800

    add dependency injection module

---
+++ learning-in-spring/dependency-injection/src/main/java/com/wfr/learning/dependency/injection/XmlDependencySetterInjectionDemo.java
@@ -0,0 +1,14 @@
+package com.wfr.learning.dependency.injection;
+
+/**
+ * 基于 XML 资源的依赖 Setter 方法注入示例
+ *
+ * @author wangfarui
+ * @since 2022/2/15
+ */
+public class XmlDependencySetterInjectionDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
