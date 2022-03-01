b8fad9e1f40df9ad8920902629dd8a5274244d04
WangFaRui <970201006@qq.com>
2022-03-01 14:55:34 +0800

    init bean-scope module

---
+++ learning-in-spring/bean-scope/src/main/java/com/wfr/learning/bean/scope/BeanScopeDemo.java
@@ -0,0 +1,14 @@
+package com.wfr.learning.bean.scope;
+
+/**
+ * Spring Bean作用域示例
+ *
+ * @author wangfarui
+ * @since 2022/3/1
+ */
+public class BeanScopeDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
