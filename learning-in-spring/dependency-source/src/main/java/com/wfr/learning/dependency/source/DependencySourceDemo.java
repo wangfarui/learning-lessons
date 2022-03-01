ddffe9cc5ae4cb9e3b80045f299e8ff2559da2d5
WangFaRui <970201006@qq.com>
2022-03-01 14:55:04 +0800

    init dependency-source module

---
+++ learning-in-spring/dependency-source/src/main/java/com/wfr/learning/dependency/source/DependencySourceDemo.java
@@ -0,0 +1,14 @@
+package com.wfr.learning.dependency.source;
+
+/**
+ * 依赖来源示例
+ *
+ * @author wangfarui
+ * @since 2022/3/1
+ */
+public class DependencySourceDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
