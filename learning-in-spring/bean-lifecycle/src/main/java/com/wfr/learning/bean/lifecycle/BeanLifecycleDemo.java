633f92868b4a4f245f0d949cd85db8166542fd9b
WangFaRui <970201006@qq.com>
2022-03-01 14:58:17 +0800

    init bean-lifecycle module

---
+++ learning-in-spring/bean-lifecycle/src/main/java/com/wfr/learning/bean/lifecycle/BeanLifecycleDemo.java
@@ -0,0 +1,14 @@
+package com.wfr.learning.bean.lifecycle;
+
+/**
+ * Spring Bean生命周期示例
+ *
+ * @author wangfarui
+ * @since 2022/3/1
+ */
+public class BeanLifecycleDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
