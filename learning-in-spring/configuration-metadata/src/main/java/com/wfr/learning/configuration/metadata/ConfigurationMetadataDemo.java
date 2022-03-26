e7aae01b145c34ef2ac94fccea4e6f0213474ab8
WangFaRui <970201006@qq.com>
2022-03-26 14:35:11 +0800

    add configuration-metadata module

---
+++ learning-in-spring/configuration-metadata/src/main/java/com/wfr/learning/configuration/metadata/ConfigurationMetadataDemo.java
@@ -0,0 +1,14 @@
+package com.wfr.learning.configuration.metadata;
+
+/**
+ * Spring Bean配置元信息示例
+ *
+ * @author wangfarui
+ * @since 2022/3/26
+ */
+public class ConfigurationMetadataDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
