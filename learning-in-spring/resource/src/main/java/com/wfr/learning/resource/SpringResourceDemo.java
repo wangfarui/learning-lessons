db9d6e3d0494b1c014418260fccc86ac7fec167d
WangFaRui <970201006@qq.com>
2022-03-28 18:18:31 +0800

    add resource module

---
+++ learning-in-spring/resource/src/main/java/com/wfr/learning/resource/SpringResourceDemo.java
@@ -0,0 +1,14 @@
+package com.wfr.learning.resource;
+
+/**
+ * 基于 {@link org.springframework.core.io.Resource} 资源管理示例
+ *
+ * @author wangfarui
+ * @since 2022/3/28
+ */
+public class SpringResourceDemo {
+
+    public static void main(String[] args) {
+        
+    }
+}
