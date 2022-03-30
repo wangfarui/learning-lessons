ddce1d1c04a966baa8b4e99ffb3267d68712add2
WangFaRui <970201006@qq.com>
2022-03-30 10:35:56 +0800

    add validation module

---
+++ learning-in-spring/validation/src/main/java/com/wfr/learning/in/spring/validation/ValidatorDemo.java
@@ -0,0 +1,14 @@
+package com.wfr.learning.in.spring.validation;
+
+/**
+ * 自定义 {@link org.springframework.validation.Validator} 校验示例
+ *
+ * @author wangfarui
+ * @since 2022/3/30
+ */
+public class ValidatorDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
