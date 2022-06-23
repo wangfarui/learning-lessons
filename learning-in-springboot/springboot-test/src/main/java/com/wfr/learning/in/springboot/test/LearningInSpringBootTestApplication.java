9dd02a41b4c44b07c7fa1b1f8f995cc044fb72dd
WangFaRui <wangfarui163@163.com>
2022-06-23 17:11:04 +0800

    improve spring.profiles.active description

--- learning-in-springboot/springboot-test/src/main/java/com/wfr/learning/in/springboot/test/LearningInSpringBootTestApplication.java
+++ learning-in-springboot/springboot-test/src/main/java/com/wfr/learning/in/springboot/test/LearningInSpringBootTestApplication.java
@@ -17,6 +17,8 @@ public class LearningInSpringBootTestApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(LearningInSpringBootTestApplication.class);
         ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        // 测试 spring.profiles.active 动态多配置源
+        // 基于 @xxx@ 形式, pom文件需要指定<profiles>标签, 并且装有对应的插件
         String applicationName = environment.getProperty("spring.application.name");
         String id = environment.getProperty("wfr.id");
         System.out.println(applicationName + " - " + id);
