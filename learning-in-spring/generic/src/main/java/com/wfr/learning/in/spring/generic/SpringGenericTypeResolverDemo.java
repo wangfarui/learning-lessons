3ca2b0700da09c3ed28fb4d856cd9fcd646ece70
WangFaRui <wangfarui163@163.com>
2022-06-29 17:12:07 +0800

    update SpringGenericTypeResolverDemo.java

--- learning-in-spring/generic/src/main/java/com/wfr/learning/in/spring/generic/SpringGenericTypeResolverDemo.java
+++ learning-in-spring/generic/src/main/java/com/wfr/learning/in/spring/generic/SpringGenericTypeResolverDemo.java
@@ -6,12 +6,11 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
 /**
- * description
+ * 基于 {@link GenericTypeResolver} 示例
  *
  * @author wangfarui
  * @since 2022/6/29
